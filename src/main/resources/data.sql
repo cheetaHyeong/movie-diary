73c6288a431f7b78f50cf6744f439ec6b28bd225
CTH128 <163491242+cheetaHyeong@users.noreply.github.com>
2025-05-24 20:16:03 +0900

    sql 데이터 추가 css 파일 수정 list 조회 페이지 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,9 @@ INSERT INTO movie (title, poster_photo, watched_date, release_date, rating, revi
 VALUES
 ('스파이더맨', 'https://image.cine21.com/resize/IMGDB/poster/2002/0401/large/093014_spide[X280,400].JPG', '2024-05-01', '2021-12-15', 9.0, '마블 최고의 액션', true),
 ('인셉션', 'https://image.cine21.com/resize/cine21/poster/2023/0404/18_44_44__642bf18c78d85[X280,400].jpg', '2024-04-20', '2010-07-21', 8.8, '꿈속의 꿈은 현실인가', false),
-('기생충', 'https://image.cine21.com/resize/cine21/poster/2019/0528/15_09_56__5cecd0b4d4e0c[X280,400].jpg', '2024-03-15', '2019-05-30', 9.5, '가족 드라마의 역작', true);
+('기생충', 'https://image.cine21.com/resize/cine21/poster/2019/0528/15_09_56__5cecd0b4d4e0c[X280,400].jpg', '2024-03-15', '2019-05-30', 9.5, '가족 드라마의 역작', true),
+('인터스텔라', 'https://image.cine21.com/resize/cine21/poster/2016/0106/10_45_37__568c71c1f23eb[X280,400].jpg', '2024-05-20', '2014-11-06', 9.6, '우주와 시간의 경이로움', true),
+('라라랜드', 'https://image.cine21.com/resize/cine21/poster/2020/1230/14_43_54__5fec139a08a7e[X280,400].jpg', '2024-05-17', '2016-12-07', 8.5, '화려한 음악과 사랑 이야기', false),
+('해리 포터와 마법사의 돌', 'https://image.cine21.com/resize/cine21/poster/2018/1019/14_23_20__5bc96a483218d[X280,400].jpg', '2024-05-15', '2001-12-13', 9.1, '마법 세계의 시작', true),
+('어벤져스: 엔드게임', 'https://image.cine21.com/resize/cine21/poster/2019/0417/11_38_55__5cb691bfbbfd0[X280,400].jpg', '2024-05-10', '2019-04-24', 8.9, '히어로들의 마지막 전투', true),
+('월-E', 'https://image.cine21.com/resize/cine21/poster/2008/0724/M0010003__E-1[X280,400].jpg', '2024-05-05', '2008-08-21', 8.4, '지구를 지키는 작은 로봇 이야기', false);
